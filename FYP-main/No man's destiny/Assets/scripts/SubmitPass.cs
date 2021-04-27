61bf33a3f58e440670cd7042a61034ab688650a2
ArslanAK47 <60480680+ArslanAK47@users.noreply.github.com>
2021-04-27 10:58:02 +0500

    PUSHED

---
+++ FYP-main/No man's destiny/Assets/scripts/SubmitPass.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SubmitPass : MonoBehaviour
+{
+    private string name1;
+    private string password1;
+    public GameObject inputField;
+    public GameObject password;
+    void Start()
+    {
+
+
+    }
+    public void StoreName() {
+        name1 = inputField.GetComponent<TMP_InputField>().text;
+        password1 = password.GetComponent<TMP_InputField>().text;
+        Debug.Log("my name iss : " + name1);
+        Debug.Log("my password is : " + password1);
+
+    }
+
+    public void LoadUser()
+    {
+        name1 = inputField.GetComponent<TMP_InputField>().text;
+        password1 = password.GetComponent<TMP_InputField>().text;
+        Debug.Log("my name iss : " + name1);
+        Debug.Log("my password is : " + password1);
+    }
+}
+   
+    
+
